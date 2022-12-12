96a812d43cde20719a52bad64e9024b1bcee52bd
saleamlak <96423341+saleamlakw@users.noreply.github.com>
2022-12-12 11:15:35 -0800

    Update design_circular_deque.py

--- design_circular_deque.py
+++ design_circular_deque.py
@@ -2,7 +2,6 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         ans=[]
         for i in nums1:
-            try:
                 f=0
                 for h in nums2[(nums2.index(i))+1:]:
                     if h>i:
@@ -11,6 +10,4 @@ class Solution:
                         break
                 if i>f:
                     ans.append(-1)
-            except:
-                ans.append(-1)
         return ans
